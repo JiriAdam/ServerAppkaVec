c4ea4102a7013a47e81f7cac9ce9f8f9a6a8ff8b
avilx <avilx@seznam.cz>
2014-11-02 13:11:22 +0100

    sample packaging

---
+++ WebServer/src/main/java/mybatis/handler/LocalDateTypeHandler.java
@@ -0,0 +1,54 @@
+package mybatis.handler;
+
+import java.sql.CallableStatement;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
+
+/**
+ * Type handler for {@link LocalDate} class
+ * @author michal.chudy
+ * @date 12.8.2013
+ */
+public class LocalDateTypeHandler extends BaseTypeHandler<LocalDate> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, LocalDate parameter, JdbcType jdbcType) throws SQLException {
+        ps.setTimestamp(i, new Timestamp(parameter.toDate().getTime()));
+    }
+
+    @Override
+    public LocalDate getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        Date date = rs.getDate(columnName);
+        if (date != null) {
+            return new LocalDate(date.getTime(), DateTimeZone.UTC);
+        }
+        return null;
+    }
+
+    @Override
+    public LocalDate getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        Date date = rs.getDate(columnIndex);
+        if (date != null) {
+            return new LocalDate(date.getTime(), DateTimeZone.UTC);
+        }
+        return null;
+    }
+
+    @Override
+    public LocalDate getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        Date date = cs.getDate(columnIndex);
+        if (date != null) {
+            return new LocalDate(date.getTime(), DateTimeZone.UTC);
+        }
+        return null;
+    }
+
+}
