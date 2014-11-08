ac18384e522df6fcf97825dd78b46598e4436157
avilx <avilx@seznam.cz>
2014-11-08 14:42:34 +0100

    app setup

--- WebServer/src/main/java/web/controller/SampleLoginLogOutOrWhatEverController.java
+++ WebServer/src/main/java/web/controller/SampleLoginLogOutOrWhatEverController.java
@@ -1,10 +1,24 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/index.html")
 public class SampleLoginLogOutOrWhatEverController {
+
+
+	@RequestMapping(method=RequestMethod.GET)
+    protected String gotoIndex(Model model) throws Exception {      
+        return "index";
+    }
+	
+}
+
+
 
 	
 	
-}
+
